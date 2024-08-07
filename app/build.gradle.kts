1682d9405a031c14061d4282e244f0268a42089c
Maria <45589664+emepox@users.noreply.github.com>
2024-08-07 16:04:18 +0200

    Updated Search bar component

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation("androidx.compose.material3:material3:1.3.0-beta04")
     implementation(libs.androidx.navigation.compose)
     implementation("io.coil-kt:coil-compose:2.7.0")
     testImplementation(libs.junit)
--- app/src/main/java/com/example/funnycreaturesapp/ui/screens/Search.kt
+++ app/src/main/java/com/example/funnycreaturesapp/ui/screens/Search.kt
@@ -2,11 +2,13 @@ package com.example.funnycreaturesapp.ui.screens
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -25,16 +27,24 @@ fun Search(
         viewModel(factory = SearchViewModel.searchViewModelFactory(listOfArticles))
 
     var query by remember { mutableStateOf("") }
+    var expanded by rememberSaveable { mutableStateOf(false) }
 
     SearchBar(
-        query = query,
-        onQueryChange = {
-            viewModel.resetFilters()
-            query = it
+        inputField = {
+            SearchBarDefaults.InputField(
+                query = query,
+                onQueryChange = {
+                    viewModel.resetFilters()
+                    query = it
+                },
+                expanded = true,
+                onExpandedChange = { expanded = it },
+                onSearch = { viewModel.filterArticlesByQuery(query) },
+                placeholder = { Text(text = "Search") },
+            )
         },
-        onSearch = { viewModel.filterArticlesByQuery(query) },
-        active = true,
-        onActiveChange = { viewModel.filterArticlesByQuery(query) },
+        expanded = true,
+        onExpandedChange = { expanded = it}
     ) {
         Articles(
             articles = viewModel.filterArticlesWhileTyping(query),
